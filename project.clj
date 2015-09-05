17efb3dc82d51884e2e2d8e4908ac5d57f82db82
papachan <danloaiza@yahoo.com>
2015-09-05 18:49:18 -0500

    adding project config

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject clj-webdriver-tutorial "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler clj-webdriver-tutorial.handler/app}
+  :profiles
+  {:dev {:dependencies [[clj-webdriver "0.7.1"]
+                        [org.seleniumhq.selenium/selenium-server "2.47.0"]
+                        [javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]
+                        [ring/ring-jetty-adapter "1.4.0"]]}})
+
+
+
+
